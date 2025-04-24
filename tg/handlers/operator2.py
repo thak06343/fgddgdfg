327a8c48522c706cb3005a4a759867b6136c39fc
Твоё Имя <.email@example.com>
2025-04-24 08:08:38 +0600

    gfg

--- tg/handlers/operator2.py
+++ tg/handlers/operator2.py
@@ -157,7 +157,7 @@ async def shop_operator_mode(msg: Message, state: FSMContext):
         user = await sync_to_async(TGUser.objects.get)(user_id=msg.from_user.id)
         shop_operator = await sync_to_async(ShopOperator.objects.get)(operator=user, active=True)
         new_operator_mode = await sync_to_async(OperatorMode.objects.create)(req=req, max_amount=usdt_amount)
-        await state.update_data(mode_id=new_operator_mode.id, shop_id=shop_operator.shop.id, req=req.id)
+        await state.update_data(mode_id=new_operator_mode.id, shop_id=shop_operator.shop.id, req_id=req.id)
         await state.set_state(OperatorModeState.in_mode)
         await msg.answer(text, reply_markup=shop_operator_bottoms)
         await msg.answer(text2)
